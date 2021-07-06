46dea4bef51716b6b3443184fef96d102ac2f63b
tothzalan <toth.zalan03@gmail.com>
2021-07-06 18:53:32 +0200

    Divs instead of line breaks

--- generator.cpp
+++ generator.cpp
@@ -22,42 +22,52 @@ Generator::Generator(Tokens tokens, std::string destFile) {
     this->generate(tokens, destFile);
 }
 
+std::string createAnchor(std::string href, std::string text) {
+    std::string tag = "\t\t\t<a href=\"";
+    tag += href;
+    tag += "\">";
+    tag += text;
+    tag += "</a>\n";
+    return tag;
+}
+
+std::string createParagraph(std::string text) {
+    std::string tag = "\t\t\t<p>";
+    tag += text;
+    tag += "</p>\n";
+    return tag;
+}
+
 void Generator::generate(Tokens tokens, std::string destFile) {
     std::ofstream file;
     file.open(destFile);
     file << docStart;
 
     std::vector<std::string> textsBuffer;
+    bool isNewLine = true;
     for(int i = 0; i < tokens.tokens.size(); i++) {
+        if(isNewLine) {
+            file << "\t\t<div>\n";
+            isNewLine = false;
+        }
         switch(tokens.tokens[i].type) {
             case TokenType::EOL:
                 if(textsBuffer.size() == 2) {
-                    file << "\t\t<a href=\"";
-                    file << textsBuffer[1];
-                    file << "\">";
-                    file << textsBuffer[0];
-                    file << "</a>\n";
+                    file << createAnchor(textsBuffer[1], textsBuffer[0]);
                 }
                 else if(textsBuffer.size() > 0) {
-                    file << "\t\t<p>";
-                    file << textsBuffer[0];
-                    file << "</p>\n";
+                    file << createParagraph(textsBuffer[0]);
                 }
                 textsBuffer.clear();
-                file << "\t\t<br/>\n";
+                isNewLine = true;
+                file << "\t\t</div>\n";
                 break;
             case TokenType::SEP:
                 if(textsBuffer.size() == 2) {
-                    file << "\t\t<a href=\"";
-                    file << textsBuffer[1];
-                    file << "\">";
-                    file << textsBuffer[0];
-                    file << "</a>\n";
+                    file << createAnchor(textsBuffer[1], textsBuffer[0]);
                 }
                 else if(textsBuffer.size() > 0) {
-                    file << "\t\t<p>";
-                    file << textsBuffer[0];
-                    file << "</p>\n";
+                    file << createParagraph(textsBuffer[0]);
                 }
                 textsBuffer.clear();
                 break;
