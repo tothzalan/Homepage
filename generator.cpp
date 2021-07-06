cc2173e5d4a982ad02d4da91ccc1f6b11dbaddc5
tothzalan <toth.zalan03@gmail.com>
2021-07-06 16:27:14 +0200

    Basic HTML generation the generator now functions on a very basic level

--- generator.cpp
+++ generator.cpp
@@ -1,27 +1,71 @@
 #include "generator.h"
 
+//TODO: template file support
+const char* docStart =
+    "<!DOCTYPE html>\n"
+    "<html>\n\t<head>\n"
+    "\t\t<title>Homepage</title>\n"
+    "\t</head>\n\t<body>\n";
+
+const char* docEnd =
+    "\t</body>\n<style>\n"
+    "\thtml, body {\n"
+    "\t\twidth: 80vw;\n"
+    "\t\tmargin: 0 auto;\n\t}\n"
+    "\tdiv * {\n"
+    "\t\tmargin: 10px;\n"
+    "\t\tdisplay: inline;\n"
+    "\t}\n</style>\n"
+    "</html>";
+
 Generator::Generator(Tokens tokens, std::string destFile) {
     this->generate(tokens, destFile);
 }
 
-
 void Generator::generate(Tokens tokens, std::string destFile) {
     std::ofstream file;
     file.open(destFile);
-    for(Token token : tokens.tokens) {
-        switch(token.type) {
+    file << docStart;
+
+    std::vector<std::string> textsBuffer;
+    for(int i = 0; i < tokens.tokens.size(); i++) {
+        switch(tokens.tokens[i].type) {
             case TokenType::EOL:
-                file << "\n";
+                if(textsBuffer.size() == 2) {
+                    file << "\t\t<a href=\"";
+                    file << textsBuffer[1];
+                    file << "\">";
+                    file << textsBuffer[0];
+                    file << "</a>\n";
+                }
+                else if(textsBuffer.size() > 0) {
+                    file << "\t\t<p>";
+                    file << textsBuffer[0];
+                    file << "</p>\n";
+                }
+                textsBuffer.clear();
+                file << "\t\t<br/>\n";
                 break;
             case TokenType::SEP:
-                file << " | ";
-                break;
-            case TokenType::CHG:
-                file << " ; ";
+                if(textsBuffer.size() == 2) {
+                    file << "\t\t<a href=\"";
+                    file << textsBuffer[1];
+                    file << "\">";
+                    file << textsBuffer[0];
+                    file << "</a>\n";
+                }
+                else if(textsBuffer.size() > 0) {
+                    file << "\t\t<p>";
+                    file << textsBuffer[0];
+                    file << "</p>\n";
+                }
+                textsBuffer.clear();
                 break;
             case TokenType::TEXT:
-                file << token.value;
+                textsBuffer.push_back(tokens.tokens[i].value);
+                break;
         }
     }
+    file << docEnd;
     file.close();
 }
