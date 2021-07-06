cfe1888dc4b445e9059be89174fb13f9137b7f6a
tothzalan <toth.zalan03@gmail.com>
2021-07-06 19:00:35 +0200

    Text trimming

--- token.cpp
+++ token.cpp
@@ -9,27 +9,41 @@ Tokens::Tokens(std::vector<std::string> lines) {
     this->tokens = this->tokenizer(lines);
 }
 
+std::string trim(std::string input) {
+    auto start = input.begin();
+    while (start != input.end() && std::isspace(*start)) {
+        start++;
+    }
+
+    auto end = input.end();
+    do {
+        end--;
+    } while (std::distance(start, end) > 0 && std::isspace(*end));
+
+    return std::string(start, end + 1);
+}
+
 std::vector<Token> Tokens::tokenizer(std::vector<std::string> lines) {
     std::vector<Token> tokens;
     for(int i = 0; i < lines.size(); i++) {
         std::string buffer = "";
         for(char letter : lines[i]) {
             if(letter == '|') { //SEP
-                tokens.push_back(Token(TokenType::TEXT, buffer));
+                tokens.push_back(Token(TokenType::TEXT, trim(buffer)));
                 tokens.push_back(Token(TokenType::SEP));
                 buffer = "";               
             }
             else if(letter == ';') { //CHG
-                tokens.push_back(Token(TokenType::TEXT, buffer));
+                tokens.push_back(Token(TokenType::TEXT, trim(buffer)));
                 tokens.push_back(Token(TokenType::CHG));
                 buffer = "";
             }
             else {
-                buffer.push_back(letter); 
+                buffer.push_back(letter);
             }
         }
         if(buffer.size() > 0) {
-            tokens.push_back(Token(TokenType::TEXT, buffer));
+            tokens.push_back(Token(TokenType::TEXT, trim(buffer)));
         }
         tokens.push_back(Token(TokenType::EOL));
     }
