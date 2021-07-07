6dc8f61c49d82c79f59c959393162bc4380c1aa9
zalan <zalan@localhost.localdomain>
2021-07-07 18:31:40 +0200

    File error handling

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ int main(int argc, char* argv[]) {
         return 22;
     }
     std::ifstream sourceFile(argv[1]);
+    if(!sourceFile) {
+        std::cout << "File not found" << std::endl;
+        return 1;
+    }
     std::string line;
     std::vector<std::string> sourceLines;
     while(std::getline(sourceFile, line)) {
