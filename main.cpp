302caa25a6e6b319c8fdf3faa5dbec30cb69673d
tothzalan <toth.zalan03@gmail.com>
2021-07-07 18:35:38 +0200

    Deleting useless comment

--- main.cpp
+++ main.cpp
@@ -22,24 +22,6 @@ int main(int argc, char* argv[]) {
         sourceLines.push_back(line);
     }
     Tokens tokens(sourceLines);
-    /*
-    for(Token token : tokens.tokens) {
-        switch(token.type) {
-            case TokenType::EOL:
-                std::cout << "( EOL : ";
-                break;
-            case TokenType::SEP:
-                std::cout << "( SEP: ";
-                break;
-            case TokenType::CHG:
-                std::cout << "( CHG: ";
-                break;
-            case TokenType::TEXT:
-                std::cout << "( TEXT: ";
-        }
-        std::cout << "'" << token.value << "' )" << std::endl;
-    }
-    */
     if(argc == 3)
         Generator generator(tokens, argv[2]);
     else
